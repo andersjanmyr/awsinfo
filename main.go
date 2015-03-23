77d8730c43b8c6afdcc8d6a6aa65aaee617d10bc
Anders Janmyr <anders.janmyr@sonymobile.com>
2015-03-23 18:05:11 +0100

    Fixed json parsing

--- main.go
+++ main.go
@@ -8,8 +8,8 @@ import (
 	"time"
 )
 
-func Get() (map[string]string, error) {
-	var info map[string]string
+func Get() (map[string]interface{}, error) {
+	info := make(map[string]interface{})
 	timeout := time.Duration(2 * time.Second)
 	client := http.Client{
 		Timeout: timeout,
@@ -24,14 +24,21 @@ func Get() (map[string]string, error) {
 	if err != nil {
 		return nil, err
 	}
+	log.Printf("info %#v", string(data))
 
-	err = json.Unmarshal(data, info)
+	err = json.Unmarshal(data, &info)
+	if err != nil {
+		return nil, err
+	}
+
+	log.Println("Getting hostname")
 	resp, err = client.Get("http://169.254.169.254/latest/meta-data/public-hostname")
 	if err != nil {
 		return nil, err
 	}
 	defer resp.Body.Close()
 	data, err = ioutil.ReadAll(resp.Body)
+	log.Println("public-hostname", string(data))
 	if err != nil {
 		return nil, err
 	}
